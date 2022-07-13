7723dd67cd59fbc3ee8fa5ebac9f089913e732a0
saintpvul <saintpvul@gmail.com>
2022-07-13 19:26:11 +0600

    update: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let openBrackets = [];
+  let closeBrackets = [];
+  bracketsConfig.forEach((brackets) => {
+    openBrackets.push(brackets[0]);
+    closeBrackets.push(brackets[1]);
+  });
+
+  function closeOpened(char) {
+    if (opened[opened.length - 1] === openBrackets[closeBrackets.indexOf(char)])
+      opened.pop();
+  }
+
+  let opened = [];
+
+  for (let char of str) {
+    if (
+      closeBrackets.includes(char) &&
+      opened.includes(openBrackets[closeBrackets.indexOf(char)])
+    ) {
+      closeOpened(char);
+    } else if (openBrackets.includes(char)) {
+      opened.push(char);
+    } else return false;
+  }
+
+  return opened.length ? false : true;
+};
+// complete task
